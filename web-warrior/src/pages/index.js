d5ec17d7e603d5f929d1b20e9a75df010f968b0f
99lalo <74028591+99lalo@users.noreply.github.com>
2021-05-23 17:08:50 -0400

    Middle

--- web-warrior/src/pages/index.js
+++ web-warrior/src/pages/index.js
@@ -1,15 +1,21 @@
 import React from "react"
+import { header, btn } from "../styles/home.module.css"
 import Layout from "../components/Layout"
+import { Link } from "gatsby"
 export default function Home() {
   return (
     <Layout>
-    <section>
-      <div>
-        <h2>Design</h2>
-        <h3>Develop & Deploy</h3>
-        <p>UX designer & web developer based in Miami.</p>
-      </div>
-    </section>
+      <section className={header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in Miami.</p>
+        </div>
+        <img src="/banner.png" alt="" style={{ maxWidth: "100%" }} />
+      </section>
+      <Link className={btn} to="/projects">
+        My Portfolio Projects
+      </Link>
     </Layout>
   )
 }
