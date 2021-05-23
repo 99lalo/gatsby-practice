7cf6e6460c02184997f39011b8281075394eff6a
99lalo <74028591+99lalo@users.noreply.github.com>
2021-05-23 13:49:35 -0400

    Start

---
+++ web-warrior/src/pages/about.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Layout from "../components/Layout"
+export default function about() {
+  return (
+      <Layout>
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        mollitia, molestiae quas vel sint commodi repudiandae consequuntur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        mollitia, molestiae quas vel sint commodi repudiandae consequuntur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+        mollitia, molestiae quas vel sint commodi repudiandae consequuntur.
+      </p>
+    </div>
+    </Layout>
+  )
+}
---
+++ web-warrior/src/pages/proyects/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "../../components/Layout"
+
+export default function Proyects() {
+    return (
+        <Layout>
+        <div>
+        <h1>Portfolio</h1>
+        <h2>Proyects & Websites I've Created</h2>
+        </div>
+        </Layout>
+    )
+}
