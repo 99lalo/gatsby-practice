de0acfc153e52813b8c35a3dd46c015dd3dbb369
99lalo <74028591+99lalo@users.noreply.github.com>
2021-05-23 20:58:55 -0400

    Finished

--- web-warrior/src/pages/index.js
+++ web-warrior/src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 import { header, btn } from "../styles/home.module.css"
 import Layout from "../components/Layout"
-import { Link } from "gatsby"
-export default function Home() {
+import { Link, graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+export default function Home({ data }) {
   return (
     <Layout>
       <section className={header}>
@@ -10,12 +11,22 @@ export default function Home() {
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>UX designer & web developer based in Miami.</p>
+          <Link className={btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
-        <img src="/banner.png" alt="" style={{ maxWidth: "100%" }} />
+        <GatsbyImage image={data.file.childImageSharp.gatsbyImageData} />
       </section>
-      <Link className={btn} to="/projects">
-        My Portfolio Projects
-      </Link>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query Banner {
+    file(relativePath: { eq: "banner.png" }) {
+      childImageSharp {
+        gatsbyImageData(pngOptions: {})
+      }
+    }
+  }
+`
